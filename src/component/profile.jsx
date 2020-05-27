b930fe52c03b80467496fd90d6f6d18281e576a6
Luke Choi <primerajh@gmail.com>
2020-05-27 15:11:35 +0900

    resposive design still not done

--- src/component/profile.jsx
+++ src/component/profile.jsx
@@ -2,30 +2,29 @@ import React from "react";
 
 const Profile = () => {
   return (
-    <div id="profile">
+    <div id="profile" className="container">
       <div className="about-wrap">
         <p className="about">About Me</p>
       </div>
-      <div className="wrapper">
-        <div className="profile-pic">
-          <img id="profile-img" src="/images/header-background.jpg" alt="" />
-        </div>
-        <div className="title">
-          <h1>나는 최재형이다</h1>
-        </div>
-        <div className="subtitle">
-          <p>
-            I am an athlete at heart, with an entrepreneurial spirit, a knack
-            for online security, and a passion for coding. Having returned from
-            my year of traveling, I pursued my first development job to kick off
-            my career. I was hired as a full stack developer by Shop Your Own
-            Mortgage, where I still work today. Since beginning my career there,
-            I have been given the opportunity to shift into cybersecurity for
-            the company. I am currently working towards my Certified Ethical
-            Hacker certification while working as a full stack developer and
-            lead security specialist.
-          </p>
-        </div>
+
+      <div className="profile-pic">
+        <img id="profile-img" src="/images/header-background.jpg" alt="" />
+      </div>
+      <div className="title">
+        <h1>나는 최재형이다</h1>
+      </div>
+      <div className="subtitle">
+        <p>
+          I am an athlete at heart, with an entrepreneurial spirit, a knack for
+          online security, and a passion for coding. Having returned from my
+          year of traveling, I pursued my first development job to kick off my
+          career. I was hired as a full stack developer by Shop Your Own
+          Mortgage, where I still work today. Since beginning my career there, I
+          have been given the opportunity to shift into cybersecurity for the
+          company. I am currently working towards my Certified Ethical Hacker
+          certification while working as a full stack developer and lead
+          security specialist.
+        </p>
       </div>
     </div>
   );
